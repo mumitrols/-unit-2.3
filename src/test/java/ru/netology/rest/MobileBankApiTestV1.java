1d4e872cdfc5e9312b10ce9ad42700b6ce2af46e
mumitrols <mumitrols@bk.ru>
2022-04-17 19:37:38 +0300

    Добавлены файлы

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+import io.restassured.specification.Argument;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class MobileBankApiTestV1 {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
